14fb5c8ad59739a5cbaa0f63732c7f2620ad9213
Goce Kabov <Goce.Kabov@seavus.com>
2020-12-24 21:54:51 +0100

    added code from class for class 01

---
+++ G6/Class01/Code_From_Class/Code_From_Class_JS/script.js
@@ -0,0 +1,26 @@
+console.log("HELLO WORLD FROM THE CONSOLE!");
+alert("HELLO WORLD FROM THE POP UP!")
+
+// Declaration
+var firstName;
+
+// Initialization
+firstName = "John"
+
+// Declaration and Initialization
+var lastName = "Doe"
+
+// single line comment
+
+/* 
+    this is first line comment
+    this is second line comment
+*/
+
+var number1 = 10;
+var number2 = 20;
+
+// number1 = number1 + number2
+number1 +=number2;
+
+alert(number1);
