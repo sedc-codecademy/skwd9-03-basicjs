fea8bbf62f7969cdcfc97c279723b77488f493ef
Ivo Kostovski <ivo.kostovski@gmail.com>
2020-12-29 19:06:51 +0100

    add code on break

---
+++ G5/Class02/script/app.js
@@ -0,0 +1,76 @@
+// let dog = 'dog';
+// let cat = 'cat';
+
+// let animals = dog + cat;
+// let animals = dog + ', ' + cat;
+// let animals = `${dog}, ${cat}`;
+// console.log(animals);
+
+// let sentence = "It's really fun being a web developer";
+// let sentence = 'It\"s really fun being a web developer';
+// let sentence = `It's really "fun" being a web developer`;
+// let sentence = `
+// multi
+// line
+// code`;
+
+// console.log(sentence);
+
+// let year = 2020;
+// let word = 'year';
+// let result = year + word;
+
+// console.log(typeof result);
+
+// let a = 1;
+// let b = "2";
+
+// let result = a + b; // "1" + "2"
+// console.log(result);
+
+// Non value values
+// let dog;
+// let cat = null;
+// console.log(dog);
+// console.log(cat);
+
+// let a = 2 / "foo";
+
+// // console.log(a);
+// // console.log(typeof a);
+// console.log(a);
+// console.log(isNaN('ivo'))
+// console.log(Number.isNaN('ivo'))
+
+// let a = 1 / 0;
+// console.log(a);
+
+// let b = -1 / 0;
+// console.log(b);
+
+// console.log(5 == "5") // true / checks value
+// console.log(5 === "5") // false / checks value + type
+
+// let score1 = 10;
+// let score2 = 20;
+// let hightScore1 = 5;
+// let hightScore2 = 10;
+
+// let result = ((score1 + score2) > (hightScore1 + hightScore2));
+// console.log(result);
+
+// let result = (5 < 2) && (2 >= 3);
+//             //false  &&  false
+// console.log(result);
+
+// !! - converts values to a boolean;
+
+// let fName = '';
+
+// let res = fName === '' || fName === undefined || fName === null; <-- this is falsy
+// let res = fName = 'Ivo' <-- this is truthy
+// let res = !!fName;
+
+// let dog = 'dog';
+// console.log('dasdsad', dog)
+
