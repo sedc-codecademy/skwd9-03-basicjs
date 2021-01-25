44d58f6697a2420cb4b56e110287052bb8490ac2
tstojanovska <stojanovskatanja03@gmail.com>
2021-01-25 21:17:18 +0100

    Class 09

---
+++ G2/Class09/Solutions/scripts/exercise1.js
@@ -0,0 +1,12 @@
+function cleanFalsyValues(inputArray){
+    let result = [];
+    for(let item of inputArray){
+        if(!!item){
+            result.push(item);
+        }
+    }
+    return result;
+}
+
+console.log(cleanFalsyValues([2, 5, ['a', 'b', 'c'], "sedc"]));
+console.log(cleanFalsyValues([2, NaN , 7, ['a', 'b', 'c'], null, "sedc", 0]));
