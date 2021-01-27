876263a95c2c8b213a68e52d03fa0efa8ff0193b
kristina-s <spasevska.kristina87@gmail.com>
2021-01-27 21:41:16 +0100

    G4 class 10 code and extra exercises added

---
+++ G4/ExtraExercises/ExtraExercisesClass10/ExtraClassExercises.js
@@ -0,0 +1,32 @@
+// Task 1
+// Make a simple calculator application
+// It should have two input fields of type number and five buttons
+// Every button should perform the different calculation: SUM, DIFFERENCE, MULTIPLICATION, DIVISION, SQUARE
+// You should create a function for each of them and to call it on the appropriate button click
+// After clicking the button the result should be display the result in an appropriate <p> tag.
+
+// Task 2
+// Make a simple login application
+// Declare an array named users, that will hold usernames and serve you as a fake database
+// Declare another array that will keep the passwords
+// Declare two fields one for username and one for password, and one login button
+// On login button click if the username and the password from the input field match the combination: users[i], passwords[j] print in <h2> tag “Hello username” else print “Wrong username”
+
+// Task 3
+// Write a JavaScript program that will serve as a bingo application.
+// Make an array of numbers from 1 till 37 that you will display it in a <ol> tag
+// Make another array that will have only 5 numbers and it will serve you as a winning combination
+// There should be 5 input fields for inserting numbers and a button for submitting the bingo combination
+// You should get the values from the input fields and check if all of the inserted numbers are equal to the winning combination and display it in a h1 tag winning message “You won 1000000$! Congrats!”
+// Else print “More luck next time!”
+
+// Task 4
+// Write a JavaScript page that can save recipes in a table.
+// There should be inputs for:
+// Name
+// Ingredients ( strings )
+// TimeToMake
+// The recipes in the table should have a delete button that delets the table row
+// Below the table there should be 3 buttons
+// Show recipe that takes the longest time to make, show recipe that takes the shortest time to make and show all
+// The buttons should change the table to only show that recipe or show all recipes
