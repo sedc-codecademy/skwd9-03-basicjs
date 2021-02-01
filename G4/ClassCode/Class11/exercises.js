5155e72d6e9f2392461badde09115e6ad942455a
kristina-s <spasevska.kristina87@gmail.com>
2021-02-01 20:48:40 +0100

    class exercises added

--- G4/ClassCode/Class11/exercises.js
+++ G4/ClassCode/Class11/exercises.js
@@ -6,7 +6,6 @@
 // 2. Create a javascript function that will find all numbers divisable by 7 and 3 in a array from 1 to 100.
 // 30 mins
 
-
 // 3. Create a Javascript ATM machine. 
 //The ATM should have the following functions:
 //- make desposit (this function should add money to the users balance)
@@ -15,3 +14,5 @@
 //- exit (this function should close the window)
 // create interactive menu using alerts
 // 90 mins
+
+// when using inputs in forms, if you want the page not to reload (and lose the text message) use the event parameter (event) inside the event and write event.preventDefault() in the anonymous function
